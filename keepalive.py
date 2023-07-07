27b3b1040a1b7c290f8f76569c224011adad481a
Xy90000001 <106469364+Xy90000001@users.noreply.github.com>
2023-07-07 10:32:41 -0700

    Create keepalive.py

---
+++ keepalive.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from threading import Thread
+import subprocess as s
+import time, os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+#   return render_template('index.html', value=str(f))
+  return 'botsel'
+
+def run():
+  app.run(host='0.0.0.0', port=8080, use_reloader=False, debug=True)
+  # app.run()
+
+# # import botsel
+# # botsel.botsel_thread()
+# run()
+
+def keep_alive():
+  # t = threading.Thread(target=, args = (q,u))
+  t = Thread(target=run)
+  t.start()
+  time.sleep(1)
+
+# # keep_alive()
+# # 
