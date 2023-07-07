b7c5af6d2be24ed568aac8182410f16af3a476cf
Xy90000001 <106469364+Xy90000001@users.noreply.github.com>
2023-07-07 05:59:13 -0700

    Update web.py

--- web.py
+++ web.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
-from threading import Thread
-import subprocess as s
-import time, os
+# from threading import Thread
+# import subprocess as s
+# import time, os
 
 app = Flask(__name__)
 
@@ -10,18 +10,19 @@ def home():
 #   return render_template('index.html', value=str(f))
     return 'botsel'
 
-def run():
-  app.run(host='0.0.0.0', port=8080, use_reloader=False, debug=True)
-  # app.run()
+# def run():
+#   app.run(host='0.0.0.0', port=8080, use_reloader=False, debug=True)
+#   # app.run()
 
-# import botsel
-# botsel.botsel_thread()
-run()
+# # import botsel
+# # botsel.botsel_thread()
+# run()
 
-def keep_alive():
-  # t = threading.Thread(target=, args = (q,u))
-  t = Thread(target=run)
-  t.start()
-  time.sleep(1)
+# def keep_alive():
+#   # t = threading.Thread(target=, args = (q,u))
+#   t = Thread(target=run)
+#   t.start()
+#   time.sleep(1)
 
-# keep_alive()
+# # keep_alive()
+# # 
