84d63707e7879c021c59415ef1e73c6b0ab3cb0d
xy-90000001 <133015753+xy-90000001@users.noreply.github.com>
2023-07-31 03:14:54 -0700

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -12,13 +12,14 @@ async def main():
     # help(pyppeteer.chromium_downloader.download_chromium)
     # browser = await launch()
     # browser = await launch(executablePath='./chrome-linux/chrome', disable-gpu=True, no-sandbox=True, single-process=True, disable-web-security=True, disable-dev-profile=True, headless=True )
-    browser = await launch(executablePath = './chrome-linux/chrome', headless = True)
-    # browser = await launch({'executablePath' = './chrome-linux/chrome',
+    # browser = await launch(executablePath = './chrome-linux/chrome', headless = True)
+    browser = await launch({
     #         'headless': True,
     #         'args': [
     #             '--no-sandbox', 
     #             '--disabled-setuid-sandbox', 
     #             '--disable-dev-profile', 
+                  '--executablePath' = './chrome-linux/chrome',
     #             '--user-data-dir=/tmp'
     #         ]
     #     })
