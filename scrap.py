40f173d18bb00057645f6845392fb2ca30b81c7c
xy-90000001 <133015753+xy-90000001@users.noreply.github.com>
2023-07-31 05:53:37 -0700

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -11,18 +11,12 @@ async def main():
     # pyppeteer.chromium_downloader.download_chromium()
     # help(pyppeteer.chromium_downloader.download_chromium)
     # browser = await launch()
+    browser = await launch(executablePath = './chrome-linux/chrome', userDataDir=False)
     # browser = await launch(executablePath='./chrome-linux/chrome', disable-gpu=True, no-sandbox=True, single-process=True, disable-web-security=True, disable-dev-profile=True, headless=True )
     # browser = await launch(executablePath = './chrome-linux/chrome', headless = True)
-    browser = await launch({
-             'headless': True,
-             'args': [
-                 '--no-sandbox', 
-                 '--disabled-setuid-sandbox', 
-                 '--disable-dev-profile', 
-                 '--executablePath' = './chrome-linux/chrome',
-                 '--user-data-dir=/tmp'
-             ]
-         })
+    # browser = await launch({'headless': True,'args': ['--no-sandbox', '--disabled-setuid-sandbox', '--disable-dev-profile', 
+    #              '--executablePath' = './chrome-linux/chrome',
+    #              '--user-data-dir=/tmp']})
     page = await browser.newPage()
     url = 'http://ident.me'
     await page.goto(url)
