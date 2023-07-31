c682df070f088b76e353261469906e28845a45e5
xy-90000001 <133015753+xy-90000001@users.noreply.github.com>
2023-07-31 03:18:02 -0700

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -14,15 +14,15 @@ async def main():
     # browser = await launch(executablePath='./chrome-linux/chrome', disable-gpu=True, no-sandbox=True, single-process=True, disable-web-security=True, disable-dev-profile=True, headless=True )
     # browser = await launch(executablePath = './chrome-linux/chrome', headless = True)
     browser = await launch({
-    #         'headless': True,
-    #         'args': [
-    #             '--no-sandbox', 
-    #             '--disabled-setuid-sandbox', 
-    #             '--disable-dev-profile', 
-                  '--executablePath' = './chrome-linux/chrome',
-    #             '--user-data-dir=/tmp'
-    #         ]
-    #     })
+             'headless': True,
+             'args': [
+                 '--no-sandbox', 
+                 '--disabled-setuid-sandbox', 
+                 '--disable-dev-profile', 
+                 '--executablePath' = './chrome-linux/chrome',
+                 '--user-data-dir=/tmp'
+             ]
+         })
     page = await browser.newPage()
     url = 'http://ident.me'
     await page.goto(url)
