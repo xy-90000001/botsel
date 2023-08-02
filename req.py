eb0fc191bbaeda213840d5fd0a08dbc3b938ba2a
xy-90000001 <133015753+xy-90000001@users.noreply.github.com>
2023-08-02 10:42:53 -0700

    Create req.py

---
+++ req.py
@@ -0,0 +1,11 @@
+# import requests as req
+
+url_adsite = 'https://a000.ex16.repl.co/'
+# proxies= {adf}
+# r.get(url_adsite, proxies=proxies)
+
+from requests_html import HTMLSession
+session = HTMLSession()
+for i in range(3):
+  r = session.get(url_adsite)
+  r.html.render()
