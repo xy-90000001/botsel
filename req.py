a9ae2250f843acfc1d3d1e395bbd077d20207579
xy-90000001 <133015753+xy-90000001@users.noreply.github.com>
2023-08-02 11:20:50 -0700

    Update req.py

--- req.py
+++ req.py
@@ -1,13 +1,23 @@
 print('#################################reqqqqqqqqqqqqqqqq################################')
-# import requests as req
+import requests as req
 
 url_adsite = 'https://a000.ex16.repl.co/'
-# proxies= {adf}
-# r.get(url_adsite, proxies=proxies)
+url_ad = 'https://click.a-ads.com/2199641/2/'
 
-from requests_html import HTMLSession
-session = HTMLSession()
-for i in range(3):
+/# proxies= {adf}
+# r.get(url_adsite, proxies=proxies)
+for i in range(5):
+  print(i, 'a')
+  req.get(url_adsite)
+  
+for i in range(9):
   print(i)
-  r = session.get(url_adsite)
-  r.html.render()
+  req.get(url_adsite)
+
+
+# from requests_html import HTMLSession
+# session = HTMLSession()
+# for i in range(3):
+#   print(i)
+#   r = session.get(url_adsite)
+#   r.html.render()
