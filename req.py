7d10ab612a60124d0bb9fb00ca7c0d04e1eb2bd6
xy-90000001 <133015753+xy-90000001@users.noreply.github.com>
2023-08-03 06:03:29 -0700

    Update req.py

--- req.py
+++ req.py
@@ -16,8 +16,9 @@ proxy_oxy = "http://exash:Xy90000001@unblock.oxylabs.io:60000"
 # r.get(url_adsite, proxies=proxies)
 for i in range(5):
   print(i)
+  req.get(url_ipm)
   req.get(url_ipm, verify=False, proxies=proxies)
-  sleep(1)
+  # sleep(1)
   # req.get(url_click,  verify=False,  proxies=proxies)
   
 # from requests_html import HTMLSession
