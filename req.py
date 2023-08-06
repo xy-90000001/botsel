2e04ab875b40971cd5f38c97768fe71851d3a496
xy-90000001 <133015753+xy-90000001@users.noreply.github.com>
2023-08-06 11:17:59 -0700

    Update req.py

--- req.py
+++ req.py
@@ -3,9 +3,10 @@ import requests as req
 from time import sleep
 
 url_adsite = 'https://a000.ex16.repl.co/'
-url_click = 'https://click.a-ads.com/2199641/2/'
-url_ipm = 'https://ad.a-ads.com/2199641'
-
+url_aclick = 'https://click.a-ads.com/2199641/2/'
+url_aipm = 'https://ad.a-ads.com/2199641?size=728x90'
+url_2bt = 'http://traffic2bitcoin.com/ptp2.php?ref=Exash'
+url_adsterra = 'https://www.highcpmrevenuegate.com/dahcgmdy89?key=cbcfa1e9a4c631faf6ed1b29519abfce'
 
 proxies = {'http': "http://exash:Xy90000001@unblock.oxylabs.io:60000",'https': "http://exash:Xy90000001@unblock.oxylabs.io:60000",}
 # curl 'http://quotes.toscrape.com/js/' -U 'exash:Xy90000001' -x 'unblock.oxylabs.io:60000' -H 'x-oxylabs-geo-location: United States' -H 'x-oxylabs-render: html' -k -v 
@@ -17,25 +18,26 @@ session = req.Session()
 session.proxies = proxies
 
 def mainloop():
-  for i in range(5000):
-    print(i)
-    # req.get(url_ipm)
-    session.get(url_ipm, verify=False)
+  # for i in range(5000):
+  c = 0
+  while True:
+    c+=1
+    if c==50:
+      req.get(url_ping)
+      c = 0
+    # print(i)
+    session.get(url_aipm, verify=False)
     # sleep(1)
-    session.get(url_click, verify=False)
-  
-# from requests_html import HTMLSession
-# session = HTMLSession()
-# for i in range(3):
-#   print(i)
-#   r = session.get(url_adsite)
-#   r.html.render()
-try:
-  mainloop()
-except:
-  print('err')
-    
+    # session.get(url_click, verify=False)
+    session.get(url_2bt, verify=False)
+    session.get(url_adsterra, verify=False)
 
 
 import keepalive
 keepalive.keep_alive()
+
+
+try:
+  mainloop()
+except:
+  print('err')
