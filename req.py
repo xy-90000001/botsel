5c27adc05fe5d83165206786142ad6620f480738
xy-90000001 <133015753+xy-90000001@users.noreply.github.com>
2023-08-06 11:54:35 -0700

    Update req.py

--- req.py
+++ req.py
@@ -7,6 +7,12 @@ url_aclick = 'https://click.a-ads.com/2199641/2/'
 url_aipm = 'https://ad.a-ads.com/2199641?size=728x90'
 url_2bt = 'http://traffic2bitcoin.com/ptp2.php?ref=Exash'
 url_adsterra = 'https://www.highcpmrevenuegate.com/dahcgmdy89?key=cbcfa1e9a4c631faf6ed1b29519abfce'
+url_coinserom = 'https://ads.coinserom.com/publisher?adsunit=437'
+url_adshare = 'https://app.adaround.net/supply/register?iid=LSPCjcKqClgpTzcWw67CsXs8w649'
+
+# <div class="_fa7cdd4c68507744" data-placement="4803df9cffe6498ea6fff5948dd09854" style="width:320px;height:100px;display: inline-block;margin: 0 auto"></div>
+# <script type="text/javascript" src="https://app.adaround.net/main.js" async></script>
+
 
 proxies = {'http': "http://exash:Xy90000001@unblock.oxylabs.io:60000",'https': "http://exash:Xy90000001@unblock.oxylabs.io:60000",}
 # curl 'http://quotes.toscrape.com/js/' -U 'exash:Xy90000001' -x 'unblock.oxylabs.io:60000' -H 'x-oxylabs-geo-location: United States' -H 'x-oxylabs-render: html' -k -v 
