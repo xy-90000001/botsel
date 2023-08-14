1e0d3588635497402a78e638f78e90bf146e049d
xy-90000001 <133015753+xy-90000001@users.noreply.github.com>
2023-08-14 11:20:02 -0700

    Update req.py

--- req.py
+++ req.py
@@ -25,7 +25,7 @@ proxy_apy = f'http://<{user}>:<{password2}>@proxy.apify.com:8000'
 proxies = {'http': proxy_apy, 'https':proxy_apy,}
 # proxies = {'http': "http://Xy90000001req2:Xy90000001req2@unblock.oxylabs.io:60000",'https': "http://Xy90000001req2:Xy90000001req2@unblock.oxylabs.io:60000",}
 # curl 'http://quotes.toscrape.com/js/' -U 'exash:Xy90000001' -x 'unblock.oxylabs.io:60000' -H 'x-oxylabs-geo-location: United States' -H 'x-oxylabs-render: html' -k -v 
-proxy_oxy = "http://Cache0000:Cache12341234@unblock.oxylabs.io:60000"
+proxy_oxy = "http://Cache0000:Cache000@unblock.oxylabs.io:60000"
 proxies = {'http': proxy_oxy, 'https':proxy_oxy,}
 # auth = HTTPProxyAuth(user, password2)
 
